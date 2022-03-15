b080478f094f7fb10f54993a45af4b9fef8a43c0
alwx174 <alex.gertskovich@post.idc.ac.il>
2022-03-15 19:13:15 +0200

    vector-implemented gradient magnitude

--- ex1/ex1/seam_carving.py
+++ ex1/ex1/seam_carving.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math
 def get_greyscale_image(image, colour_wts):
     """
     Gets an image and weights of each colour and returns the image in greyscale
@@ -18,11 +18,6 @@ def reshape_bilinear(image, new_shape):
     :param new_shape: a (height, width) tuple which is the new shape
     :returns: the image resized to new_shape
     """
-    in_height, in_width, _ = image.shape
-    out_height, out_width = new_shape
-    new_image = np.zeros(new_shape)
-    ###Your code here###
-    ###**************####
     return new_image
     
 def gradient_magnitude(image, colour_wts):
@@ -33,8 +28,47 @@ def gradient_magnitude(image, colour_wts):
     :returns: The gradient image
     """
     greyscale = get_greyscale_image(image, colour_wts)
+    # greyscale = np.array([[1, 2, 6], [3, 4, 5], [5,6,7]])
+    # test = np.square(np.gradient(np.array([[1, 2, 6], [3, 4, 5], [5,6,7]], dtype=float), axis=1)) + np.square(np.gradient(np.array([[1, 2, 6], [3, 4, 5], [5,6,7]], dtype=float), axis=0))
+    # test = np.sqrt(test)
+
+    # moving the rows to prepare I(x+1,y) and I(x, y+1)
+    x_forward = np.roll(greyscale, -1 ,axis=0)
+    y_forward = np.roll(greyscale, -1 ,axis=1)
+    # perform the subtraction I(x+1,y)- I(x,y) and I(x, y+1) - I(x,y)
+    top_x = (y_forward - greyscale)
+    top_y = (x_forward - greyscale)
+
+    # top_x = ((y_forward - greyscale) + 255) * 1/2
+    # top_y = ((x_forward - greyscale) + 255) * 1/2
+    
+    # perform addition,square and root of gradient to calculate gradient magnitude
+    top_x, top_y = np.square(top_x), np.square(top_y)
+    final_res = top_x + top_y
+    final_res = np.sqrt(final_res)
+    # final_res = (final_res + 150) * (1/2)
+    gradient = final_res
+
+    # hieght, width = greyscale.shape
+    # tt = np.zeros(greyscale.shape)
+    # for i in range(hieght-1):
+    #     for j in range(width-1):
+    #         tt[i,j] = np.square(greyscale[i+1, j] - greyscale[i, j])
+    #         tt[i,j] += np.square(greyscale[i, j+1] - greyscale[i, j])
+    #         tt[i,j] = np.sqrt(tt[i, j])
+
+    # gradient = np.square(np.gradient(greyscale, axis=1)) + np.square(np.gradient(greyscale, axis=0))
+    # gradient = np.sqrt(gradient)
+    # print(2)
+    # index_mat_x = np.ones(greyscale.shape[0])
+    # for i in range(greyscale.shape[0]):
+    #     if i == 0 or i == 1:
+    #         continue
+    #     index_mat_x = np.column_stack((index_mat_x, np.ones(greyscale.shape[0])*i))
+    # np.roll(np.power(test.A, -1*np.ones(test.A.shape)), 1,axis=0)
     ###Your code here###
     ###**************###
+    
     return gradient
     
 def visualise_seams(image, new_shape, carving_scheme, colour):
